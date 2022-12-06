417a297972c0912e695cdac29a414f1ae6d78f80
Surya Aenuganti <surya@Suryas-Pro.local>
2022-12-05 21:31:25 -0500

    extract all elements of type text

---
+++ get_text_tables.py
@@ -0,0 +1,24 @@
+# Import the necessary libraries
+from bs4 import BeautifulSoup
+import requests
+
+# Define the URL of the webpage you want to scrape
+url = "https://en.wikipedia.org/wiki/Data_science"
+
+# Make a request to the webpage to retrieve the HTML
+response = requests.get(url)
+
+# Parse the HTML using BeautifulSoup
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Find all text elements on the webpage
+text_elements = soup.find_all(text=True)
+
+# Print the text of each element
+for text_element in text_elements:
+    print(text_element.strip())
+
+# tables = soup.find_all("table")
+
+# for table in tables:
+#     print(table.prettify())
